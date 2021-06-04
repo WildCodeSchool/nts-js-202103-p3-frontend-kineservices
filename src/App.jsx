8ac8f5dec9be8c196c881a96ef86c132c4a9ef73
Guillaume <Gui.lefort.17@gmail.com>
2021-06-04 11:26:03 +0200

    init home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import Home from './components/views/Home';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
