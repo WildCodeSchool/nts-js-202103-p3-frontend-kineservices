2a83a446b23955bd3cf3f2cdd4e24d639f593e7e
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-06-09 12:31:28 +0200

    création  sign up summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/views/Home';
+import SignUpSummary from './components/views/SignUpSummary';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <SignUpSummary />
     </div>
   );
 }
