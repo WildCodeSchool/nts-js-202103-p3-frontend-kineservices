56faa81e368ec1f6727af8dc49f62da57306be5b
Ninon Maraval <maravalninon@gmail.com>
2021-06-16 16:14:16 +0200

    menu déroulant des catégories dans documentationForm ok et test sur le front ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Home from './components/views/Home';
-import GeneralRouter from './components/commons/GeneralRouter';
 import SignUpSummary from './components/views/SignUpSummary';
 import SignUpKine from './components/views/SignUpKine';
 import DocumentationForm from './components/views/DocumentationForm';
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Home />
-      <GeneralRouter />
       <SignUpSummary />
       <SignUpKine />
       <DocumentationForm />
