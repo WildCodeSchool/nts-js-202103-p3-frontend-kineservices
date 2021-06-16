00f8b39f1da042de0b90e096e4d9c26a4db994c0
GUIGANTONDamien <guigantondamien@gmail.com>
2021-06-17 00:17:50 +0200

    Mise à jour CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Home from './components/views/Home';
 import GeneralRouter from './components/commons/GeneralRouter';
-import SignUpSummary from './components/views/SignUpSummary';
-import SignUpKine from './components/views/SignUpKine';
 
 function App() {
   return (
     <div className="App">
-      <Home />
       <GeneralRouter />
-      <SignUpSummary />
-      <SignUpKine />
     </div>
   );
 }
--- src/components/views/SignUpKine.jsx
+++ src/components/views/SignUpKine.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import avatar from '../../media/avatar.png';
 import './SignUp.css';
 
 export default function SignUpKine() {
@@ -32,173 +33,190 @@ export default function SignUpKine() {
   };
 
   return (
-    <div>
-      <div className="title" />
-      <div className="signUpForm">
-        <form onSubmit={handleSubmit}>
-          <label className="field" htmlFor="lastname">
-            <input
-              id="lastname"
-              type="text"
-              name="lastname"
-              placeholder="Nom :"
-              required
-            />
-          </label>
-          <label className="field" htmlFor="firstname">
-            <input
-              id="firstname"
-              type="text"
-              name="firstname"
-              placeholder="Prénom : "
-              required
-            />
-          </label>
-          <label className="field" htmlFor="birthdate">
-            <input
-              id="birthdate"
-              type="date"
-              name="birthdate"
-              placeholder="Date de naissance :"
-              required
-            />
-          </label>
-          <label className="field" htmlFor="email">
-            <input id="email" type="email" name="email" placeholder="email :" />
-          </label>
-          <label className="field" htmlFor="confirmEmail">
-            <input
-              id="confirmEmail"
-              type="email"
-              name="confirmEmail"
-              placeholder="confirm email :"
-              required
-            />
-          </label>
-          <label className="field" htmlFor="password">
-            <input
-              id="password"
-              type="password"
-              name="password"
-              placeholder="Mot de passe :"
-              required
-            />
-          </label>
-
-          <label className="field" htmlFor="confirmPassword">
-            <input
-              id="confirmPassword"
-              type="password"
-              name="confirmPassword"
-              placeholder="Mot de passe :"
-              required
-            />
-          </label>
-          <label className="field" htmlFor="RPPS">
-            {isKine ? (
-              <input
-                id="firstname"
-                type="number"
-                name="RPPS"
-                placeholder="RPPS : "
-                required
-              />
-            ) : (
-              <input
-                id="firstname"
-                type="number"
-                name="RPPS"
-                placeholder="RPPS : "
-              />
-            )}
-          </label>
-          <div>
-            <label className="check" htmlFor="kineCheck">
-              <input
-                type="radio"
-                id="kineCheck"
-                name="check"
-                value="kineCheck"
-                checked={isKine}
-                onChange={handleCheck}
-              />
-              Je suis un.e kiné
-            </label>
+    <div className="container-form">
+      <div>
+        <div className="signUpForm">
+          <div className="container-avatar">
+            <img className="avatar" src={avatar} alt="avatar" />
           </div>
-
-          <div>
-            <label className="check" htmlFor="companyCheck">
+          <form onSubmit={handleSubmit}>
+            <label className="field" htmlFor="lastname">
               <input
-                type="radio"
-                id="companyCheck"
-                name="check"
-                value="companyCheck"
-                checked={!isKine}
-                onChange={handleCheck}
-              />
-              Je suis une entreprise
-            </label>
-          </div>
-          <label className="field" htmlFor="siret">
-            {!isKine ? (
-              <input
-                className="siret"
-                id="siret"
+                id="lastname"
                 type="text"
-                name="siret"
-                placeholder="Siret :"
+                name="lastname"
+                placeholder="Nom :"
                 required
               />
-            ) : (
-              ''
-            )}
-          </label>
+            </label>
+            <label className="field" htmlFor="firstname">
+              <input
+                id="firstname"
+                type="text"
+                name="firstname"
+                placeholder="Prénom : "
+                required
+              />
+            </label>
+            <label className="field" htmlFor="birthdate">
+              <input
+                id="birthdate"
+                type="date"
+                name="birthdate"
+                placeholder="Date de naissance :"
+                required
+              />
+            </label>
+            <label className="field" htmlFor="email">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="email :"
+              />
+            </label>
+            <label className="field" htmlFor="confirmEmail">
+              <input
+                id="confirmEmail"
+                type="email"
+                name="confirmEmail"
+                placeholder="confirm email :"
+                required
+              />
+            </label>
+          </form>
+        </div>
+        <div className="signUpForm">
+          <form onSubmit={handleSubmit}>
+            <label className="field" htmlFor="password">
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Mot de passe :"
+                required
+              />
+            </label>
 
-          <label className="field" htmlFor="country">
-            <select id="country" name="country" required>
-              <optgroup label="Europe">
-                <option value="france">France</option>
-                <option value="espagne">Espagne</option>
-                <option value="italie">Italie</option>
-                <option value="royaume-uni">Royaume-Uni</option>
-              </optgroup>
-              <optgroup label="Amérique">
-                <option value="canada">Canada</option>
-                <option value="etats-unis">Etats-Unis</option>
-              </optgroup>
-              <optgroup label="Asie">
-                <option value="chine">Chine</option>
-                <option value="japon">Japon</option>
-              </optgroup>
-            </select>
-          </label>
+            <label className="field" htmlFor="confirmPassword">
+              <input
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirmer le mot de passe :"
+                required
+              />
+            </label>
+            <label className="field" htmlFor="RPPS">
+              {isKine ? (
+                <input
+                  id="firstname"
+                  type="number"
+                  name="RPPS"
+                  placeholder="RPPS : "
+                  required
+                />
+              ) : (
+                <input
+                  id="firstname"
+                  type="number"
+                  name="RPPS"
+                  placeholder="RPPS : "
+                />
+              )}
+            </label>
+            <div>
+              <div>
+                <label className="check" htmlFor="kineCheck">
+                  <input
+                    type="radio"
+                    id="kineCheck"
+                    name="check"
+                    value="kineCheck"
+                    checked={isKine}
+                    onChange={handleCheck}
+                  />
+                  Je suis un.e kiné
+                </label>
+              </div>
 
-          <label className="field" htmlFor="address">
-            <input
-              id="address"
-              type="text"
-              name="address"
-              placeholder="Adresse :"
-            />
-          </label>
-          <label className="field" htmlFor="phone">
-            <input
-              id="phone"
-              type="text"
-              name="phone"
-              placeholder="Téléphone :"
-            />
-          </label>
-          <label className="field" htmlFor="website">
-            <input
-              id="website"
-              type="text"
-              name="website"
-              placeholder="Site web :"
-            />
-          </label>
-          <button type="submit">Créer mon compte</button>
-        </form>
+              <div>
+                <label className="check" htmlFor="companyCheck">
+                  <input
+                    type="radio"
+                    id="companyCheck"
+                    name="check"
+                    value="companyCheck"
+                    checked={!isKine}
+                    onChange={handleCheck}
+                  />
+                  Je suis une entreprise
+                </label>
+              </div>
+            </div>
+            <label className="field" htmlFor="siret">
+              {!isKine ? (
+                <input
+                  className="siret"
+                  id="siret"
+                  type="text"
+                  name="siret"
+                  placeholder="Siret :"
+                  required
+                />
+              ) : (
+                ''
+              )}
+            </label>
+
+            <label className="field" htmlFor="country">
+              <select id="country" name="country" required>
+                <optgroup label="Europe">
+                  <option value="france">France</option>
+                  <option value="espagne">Espagne</option>
+                  <option value="italie">Italie</option>
+                  <option value="royaume-uni">Royaume-Uni</option>
+                </optgroup>
+                <optgroup label="Amérique">
+                  <option value="canada">Canada</option>
+                  <option value="etats-unis">Etats-Unis</option>
+                </optgroup>
+                <optgroup label="Asie">
+                  <option value="chine">Chine</option>
+                  <option value="japon">Japon</option>
+                </optgroup>
+              </select>
+            </label>
+
+            <label className="field" htmlFor="address">
+              <input
+                id="address"
+                type="text"
+                name="address"
+                placeholder="Adresse :"
+              />
+            </label>
+            <label className="field" htmlFor="phone">
+              <input
+                id="phone"
+                type="text"
+                name="phone"
+                placeholder="Téléphone :"
+              />
+            </label>
+            <label className="field" htmlFor="website">
+              <input
+                id="website"
+                type="text"
+                name="website"
+                placeholder="Site web :"
+              />
+            </label>
+            <button className="button-signup" type="submit">
+              Créer mon compte
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
