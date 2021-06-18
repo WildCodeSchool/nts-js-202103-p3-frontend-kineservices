4ca78a4f9f9c7aa9a5bac7b8c36dd7de2806cf20
Ninon Maraval <maravalninon@gmail.com>
2021-06-18 10:48:08 +0200

    doc item and list ok getting info from db

--- src/components/views/DocumentationList.jsx
+++ src/components/views/DocumentationList.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DocumentationItem from './DocumentationItem';
 
 function DocumentationList() {
-  return <div />;
+  const [documentations, setDocumentations] = useState([]);
+  useEffect(function () {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/documentation`)
+      .then((response) => {
+        setDocumentations(response.data);
+        console.log(documentations);
+      });
+  }, []);
+  return (
+    <div>
+      {documentations.map((documentation) => {
+        return (
+          <DocumentationItem
+            title={documentation.title}
+            description={documentation.description}
+            category={documentation.category_id}
+            price={documentation.price}
+            key={documentation.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default DocumentationList;
