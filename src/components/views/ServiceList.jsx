3027af3cdffc89e0b78336ee38ee5d8bb31a822d
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-06-23 13:32:26 +0200

    service list ok - service form ok

---
+++ src/components/views/ServiceList.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ServiceItem from './ServiceItem';
+
+const ServiceList = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/service`)
+      .then((response) => {
+        setServices(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      {services.map((service) => {
+        return (
+          <ServiceItem
+            title={service.title}
+            description={service.description}
+            website={service.website}
+            price={service.price}
+            key={service.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ServiceList;
