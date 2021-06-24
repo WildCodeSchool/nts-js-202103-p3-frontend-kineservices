614e26a31af28e8e86baf48af5c8e846ccc4fc01
Ninon Maraval <maravalninon@gmail.com>
2021-06-24 16:19:52 +0200

    debut css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import SignIn from './components/views/SignIn';
-import GeneralRouter from './components/commons/GeneralRouter';
 
 function App() {
   return (
     <div className="App">
       <SignIn />
-      <GeneralRouter />
     </div>
   );
 }
