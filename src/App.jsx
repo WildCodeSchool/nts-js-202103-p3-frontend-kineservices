bcd34aa568f5b7a3455cb7f7d97a3980d663993a
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-07-02 13:36:00 +0200

    création du formulaire de creation de formation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GeneralRouter from './components/commons/GeneralRouter';
+import FormationForm from './components/views/FormationForm';
 // import DocumentationList from './components/views/DocumentationList';
 // import Home from './components/views/Home';
 // import SignIn from './components/views/SignIn';
@@ -7,6 +8,7 @@ import GeneralRouter from './components/commons/GeneralRouter';
 function App() {
   return (
     <div className="App">
+      <FormationForm />
       <GeneralRouter />
     </div>
   );
