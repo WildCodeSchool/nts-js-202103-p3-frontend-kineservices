17a955bcd5afa4f5bf9f466f408caa8ce4f4174e
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-07-05 12:14:50 +0200

    footer mobile & desktop, reste probleme puces sur desktop

--- src/components/commons/Footer.jsx
+++ src/components/commons/Footer.jsx
@@ -1,7 +1,34 @@
+import './Footer.css';
+import Facebook from '../../media/facebook.svg';
+import Insta from '../../media/instagram.svg';
+import Twitter from '../../media/twitter.svg';
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Kinés.fr</h1>
+    <div className="footer">
+      <div className="networkLink">
+        <h1 className="titleKine">kinés.fr</h1>
+        <img src={Facebook} alt="facebook" className="networkLogo" />
+        <img src={Insta} alt="instagram" className="networkLogo" />
+        <img src={Twitter} alt="twitter" className="networkLogo" />
+      </div>
+      <div className="credits">
+        <p className="adress">
+          Alexandra Jacob • 01 rue adresse 37200 Tours
+          <br />
+          +33648585995 • alexandra.jacob@email.fr
+        </p>
+      </div>
+      <ul className="conditions">
+        <li className="conditionElement">
+          Conditions générales d&#039;utilisation{' '}
+        </li>
+        <li className="conditionElement">
+          {' '}
+          Politique relative à la protection des données personnelles{' '}
+        </li>
+        <li className="conditionElement"> Politique en matière de cookies</li>
+      </ul>
     </div>
   );
 };
