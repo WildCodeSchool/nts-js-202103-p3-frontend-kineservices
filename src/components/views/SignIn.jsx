9dbdf66184ba8b3e08cd963290c1480fe6e9fa78
GUIGANTONDamien <guigantondamien@gmail.com>
2021-07-05 13:56:29 +0200

    Création css page connexion

--- src/components/views/SignIn.jsx
+++ src/components/views/SignIn.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Card, Form, Col } from 'react-bootstrap';
+import background from '../../media/backgroundkine.png';
 import './SignIn.css';
 
 export default function SignIn() {
@@ -28,34 +30,64 @@ export default function SignIn() {
   };
 
   return (
-    <section className="connexion">
-      <div className="signInForm-container">
-        <form className="signInForm" onSubmit={handleSubmit}>
-          <label className="field" htmlFor="email">
-            <input
-              id="email"
-              type="email"
-              name="email"
-              onChange={handleEmail}
-              placeholder="E-mail :"
-              required
-            />
-          </label>
-          <label className="field" htmlFor="password">
-            <input
-              id="password"
-              type="password"
-              name="password"
-              onChange={handlePassword}
-              placeholder="Mot de passe : "
-              required
-            />
-          </label>
-          <button className="btn" type="submit">
-            Connexion
-          </button>
-        </form>
-      </div>
-    </section>
+    <div className="container-all">
+      <img className="background" src={background} alt="background" />
+      <section className="container-card">
+        <Card className="card-login">
+          <Card.Body className="card-body-login">
+            <div className="container-title">
+              <h1 className="title">J&apos;ai déjà un compte sur Kinés.fr</h1>
+            </div>
+            <div className="signInForm-container">
+              <form className="signInForm" onSubmit={handleSubmit}>
+                <Form.Group className="container-form">
+                  <Form.Control
+                    id="email"
+                    type="email"
+                    name="email"
+                    onChange={handleEmail}
+                    placeholder=" E-mail "
+                    required
+                  />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={handlePassword}
+                    placeholder=" Mot de passe  "
+                    required
+                  />
+                </Form.Group>
+                <div className="container-forgotpassword">
+                  <p>Mot de passe oublié ?</p>
+                </div>
+                <Form.Group className="check-validation">
+                  <Col sm={{ span: 10, offset: 2 }}>
+                    <Form.Check
+                      className="text-check"
+                      label="Se souvenir de moi"
+                    />
+                  </Col>
+                </Form.Group>
+                <div className="container-button-signIn">
+                  <button className="button-signIn" type="submit">
+                    Me connecter
+                  </button>
+                </div>
+                <hr />
+                <div className="text-newMember">
+                  <p>Je suis nouveau !</p>
+                </div>
+                <div className="inscription">
+                  <p>M&apos;inscrire</p>
+                </div>
+              </form>
+            </div>
+          </Card.Body>
+        </Card>
+      </section>
+    </div>
   );
 }
