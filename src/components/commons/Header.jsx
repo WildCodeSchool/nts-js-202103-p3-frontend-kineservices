b318004d39e1376f6fefbaab33fa258a2c1d089e
GUIGANTONDamien <guigantondamien@gmail.com>
2021-07-06 10:39:57 +0200

    Header / Header CSS ok + Link ok

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
-  return <div>hello wordl</div>;
+  return (
+    <div className="container-header">
+      <h1 className="title-header">kinés.fr</h1>
+      <div className="container-header-button">
+        <Link to="/connexion">
+          <button className="button-header-signin" type="button">
+            Me connecter
+          </button>
+        </Link>
+        <Link to="/inscription-kine">
+          <button className="button-header-signup" type="button">
+            M&apos;inscrire
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
