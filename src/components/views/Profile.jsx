181567208a4f4907607b47ed817db2133336242e
GUIGANTONDamien <guigantondamien@gmail.com>
2021-07-09 11:12:37 +0200

    Création du css de profil

--- src/components/views/Profile.jsx
+++ src/components/views/Profile.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Form, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import document from '../../media/documentation.svg';
+import service from '../../media/service.svg';
+import formation from '../../media/formation.svg';
 import './Profile.css';
 
 function Profile() {
@@ -54,44 +59,94 @@ function Profile() {
 
   return (
     <div className="Container_profil">
-      <div>
-        <h1>Mon Profil</h1>
-      </div>
       {!user ? (
         <p>loading ...</p>
       ) : (
-        <div className="container_image_avatar">
-          <div className="texte">
-            <div>prenom : {user[0].firstname}</div>
-            <div>nom : {user[0].lastname}</div>
-            <div>email: {user[0].email}</div>
-            <div>telephone : {user[0].phone}</div>
-            <div>adresse : {user[0].address}</div>
-            <div>Date de naissance :{user[0].birthdate}</div>
-            <div>Pays : {user[0].country}</div>{' '}
-            <div>site web : {user[0].website}</div>
-            <div>RPPS : {user[0].RPPS}</div>
-            <div>(role) :{user[0].role_id}</div>
-            <div>Siret : {user[0].SIRET}</div>
-          </div>
-          {/* en attente de pouvoir recuperer envoyer la photo > bdd */}
-          {/* <img
+        <Card className="card-profil">
+          <Card.Body className="card-body-profil">
+            <div className="container_image_avatar">
+              <div className="texte">
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].firstname} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].lastname} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].email} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].phone} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].address} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].birthdate} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].country} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].website} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].RPPS} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].role_id} />
+                </Form.Group>
+                <Form.Group className="container-form">
+                  <Form.Control value={user[0].SIRET} />
+                </Form.Group>
+              </div>
+              {/* en attente de pouvoir recuperer envoyer la photo > bdd */}
+              {/* <img
           src={user[0].picture}
           alt={user[0].firstname}
           className="style_avatar"
         /> */}
-        </div>
+            </div>
+          </Card.Body>
+        </Card>
       )}
+      <div className="container_bouton_logout">
+        <button type="button" className="bouton_save">
+          Enregistrer
+        </button>
+        <p>Me déconnecter</p>
+      </div>
       <div className="container_bouton">
-        <button type="button" className="bouton">
-          Partager mes connaissances
-        </button>
-        <button type="button" className="bouton">
-          Partager mes fomrations
-        </button>
-        <button type="button" className="bouton">
-          Partager mes services
-        </button>
+        <Link to="/documentation">
+          <button type="button" className="bouton">
+            <img
+              className="logo-documentation"
+              src={document}
+              alt="logo doc navbar"
+            />
+            Créer une documentation
+          </button>
+        </Link>
+        <Link to="/formation">
+          <button type="button" className="bouton">
+            <img
+              className="logo-formation"
+              src={formation}
+              alt="logo formation navbar"
+            />
+            Créer une formation
+          </button>
+        </Link>
+        <Link to="/service">
+          <button type="button" className="bouton">
+            <img
+              className="logo-service"
+              src={service}
+              alt="logo service navbar"
+            />
+            Créer un service
+          </button>
+        </Link>
       </div>
     </div>
   );
