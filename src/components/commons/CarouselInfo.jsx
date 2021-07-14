8ca51712288ae45511703e901e3643832a9579d6
Ninon Maraval <maravalninon@gmail.com>
2021-07-14 10:39:51 +0200

    changed the content of the slider

--- src/components/commons/CarouselInfo.jsx
+++ src/components/commons/CarouselInfo.jsx
@@ -5,14 +5,22 @@ import cou from '../../media/cou.jpg';
 // eslint-disable-next-line import/prefer-default-export
 export const images = [
   {
-    title: 'Tout sur les pieds',
-    subtitles: 'Arrêtons de faire des pieds et des mains',
+    title: 'Rééducation du pied',
+    subtitles:
+      'Le schéma de marche est la façon dont un patient marche. Lorsque ce schéma est perturbé, on observe une boiterie. Il existe différents types de boiterie avec un déhanchement ou une rotation externe du pied. La boiterie peut avoir plusieurs origines.',
     img: feet,
   },
-  { title: 'Tout sur le dos', subtitles: "Aïe j'ai mal", img: dos },
   {
-    title: 'Tout sur le cou',
-    subtitles: 'Le cou dans tous ses états',
+    title: 'La lombalgie',
+    subtitles:
+      'Une douleur dans le bas du dos, un mal de reins qui vous empêche le moindre mouvement, la lombalgie ou le lumbago est une affection courante. Les vertèbres lombaires sont les plus sollicitées de la colonne car elles soutiennent une part importante du poids du corps et sont au centre de nombreux mouvements de la vie quotidienne.',
+    img: dos,
+  },
+  {
+    title: 'Cervicalgie',
+    subtitles:
+      "Près de 70 % de la population adulte est amenée à souffrir de cervicalgies au cours de la vie. Dans l'immense majorité des cas, la cervicalgie est dite commune, c'est-à-dire que les examens complémentaires pratiqués (imagerie) ne montrent rien de significatif en termes d'atteinte anatomique.",
+
     img: cou,
   },
 ];
