0c95712c571adaea5746d2daa1a3c7c425119574
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-07-14 14:15:53 +0200

    style resultats SERVICE en cours

--- src/components/views/ServiceItem.jsx
+++ src/components/views/ServiceItem.jsx
@@ -1,14 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import './DocumentationItem.css';
 
 function ServiceList(props) {
   const { title, description, website, price } = props;
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>{website}</p>
-      <p>{price}</p>
+    <div className="container-docitem">
+      <div className="box-item">
+        <h1 className="title">{title}</h1>
+        <p className="description">{description}</p>
+        <p>Prix ! {price}€</p>
+        <div className="discover">
+          <a href={website} className="download-file">
+            Accéder au site !
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/views/ServiceList.jsx
+++ src/components/views/ServiceList.jsx
@@ -1,30 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ServiceItem from './ServiceItem';
+import SearchContent from '../commons/SearchContent';
+import './DocumentationList.css';
 
 const ServiceList = () => {
   const [services, setServices] = useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/service`)
       .then((response) => {
         setServices(response.data);
-      });
-  }, []);
+      }, []);
+  });
   return (
-    <div>
-      {services.map((service) => {
-        return (
-          <ServiceItem
-            title={service.title}
-            description={service.description}
-            website={service.website}
-            price={service.price}
-            key={service.id}
-          />
-        );
-      })}
-    </div>
+    <>
+      <h1 className="alldoc">Tous les services</h1>
+      <div className="search-engine">
+        <h2>Rechercher un service</h2>
+        <SearchContent
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+        <div className="documentation-results">
+          {services
+            .filter(
+              (service) =>
+                service.title.toLowerCase().includes(searchValue) ||
+                service.description.toLowerCase().includes(searchValue)
+            )
+            .map((service) => {
+              return (
+                <ServiceItem
+                  title={service.title}
+                  description={service.description}
+                  website={service.website}
+                  price={service.price}
+                  key={service.id}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </>
   );
 };
 
